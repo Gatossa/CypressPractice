67851b51f80543747eed6baa74e4ac41e99bd088
Gatossa <email@users.noreply.github.com>
2025-01-28 15:19:59 +0100

    update

--- 01 Starting Project/cypress/e2e/basics.cy.js
+++ 01 Starting Project/cypress/e2e/basics.cy.js
@@ -1,3 +1,5 @@
+/// <reference types='Cypress'/>
+
 describe("task page", () => {
   it("should render the main image", () => {
     cy.visit("http://localhost:5173/");
@@ -5,6 +7,7 @@ describe("task page", () => {
   });
   it("should display the page title", () => {
     cy.visit("http://localhost:5173/");
-    cy.contains("My Cypress Course Task");
+    cy.get("h1").contains("My Cypress Course Task");
+    cy.get("h1").should("have.length", 1);
   });
 });
