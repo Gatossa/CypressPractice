a6a8bd49059c777f51991ac321cb774106990a7b
Gatossa <email@users.noreply.github.com>
2025-01-29 11:23:41 +0100

    update

--- 01 Starting Project/cypress/e2e/tasks.cy.js
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -12,4 +12,19 @@ describe("task management", () => {
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
+
+  it("should create a new task", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("My new task");
+    cy.get("#summary").type("Summary of a new task");
+
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get(".task h2").contains("My new task");
+    cy.get(".task p").contains("Summary of a new task");
+
+    //cy.contains('Save').click();
+    //cy.contains('New Task').should('exist');
+  });
 });
