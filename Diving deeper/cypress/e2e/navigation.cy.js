e9c5b792ec61deda110cfc0c0d3f8aa88c73226d
Gatossa <email@users.noreply.github.com>
2025-01-29 15:04:40 +0100

    working working

---
+++ Diving deeper/cypress/e2e/navigation.cy.js
@@ -0,0 +1,9 @@
+/// <reference types='Cypress'/>
+
+describe("page navigation", () => {
+  it("should navigate between pages", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("[data-cy='header-about-link']").click();
+    cy.location("pathname").should("eq", "/about");
+  });
+});
