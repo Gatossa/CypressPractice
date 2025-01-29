f6d046f6ac12348aa5831dd3846d296aaad811fb
Gatossa <email@users.noreply.github.com>
2025-01-29 10:57:51 +0100

    second day

---
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -0,0 +1,15 @@
+/// <reference types='Cypress'/>
+
+describe("task management", () => {
+  it("should open and close the new task", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+    cy.contains("Add Task").click();
+    cy.contains("Cancel").click();
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+  });
+});
