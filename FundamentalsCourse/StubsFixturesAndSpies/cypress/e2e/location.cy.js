64f6ddbe568fe8b9f0fad471ff08feb75bc7c5b6
Gatossa <email@users.noreply.github.com>
2025-01-31 12:18:33 +0100

    learning Stubs

--- FundamentalsCourse/StubsFixturesAndSpies/cypress/e2e/location.cy.js
+++ FundamentalsCourse/StubsFixturesAndSpies/cypress/e2e/location.cy.js
@@ -1,8 +1,13 @@
 /// <reference types="cypress" />
 
-describe('share location', () => {
-  it('should fetch the user location', () => {
-    cy.visit('/');
+describe("share location", () => {
+  it("should fetch the user location", () => {
+    cy.visit("/").then((win) => {
+      cy.stub(win.navigator.geolocation, "getCurrentPosition").as(
+        "getCurrentUserLocalization"
+      );
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get("@getCurrentUserLocalization").should("have.been.calledOnce");
   });
 });
