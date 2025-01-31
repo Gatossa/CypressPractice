366d318c0434d3959122b2c61a91600ab76dc879
Gatossa <email@users.noreply.github.com>
2025-01-31 12:58:49 +0100

    beforeEach with Stub

--- FundamentalsCourse/StubsFixturesAndSpies/cypress/e2e/location.cy.js
+++ FundamentalsCourse/StubsFixturesAndSpies/cypress/e2e/location.cy.js
@@ -1,13 +1,34 @@
 /// <reference types="cypress" />
 
 describe("share location", () => {
-  it("should fetch the user location", () => {
+  beforeEach(() => {
     cy.visit("/").then((win) => {
-      cy.stub(win.navigator.geolocation, "getCurrentPosition").as(
-        "getCurrentUserLocalization"
-      );
+      cy.stub(win.navigator.geolocation, "getCurrentPosition")
+        .as("getCurrentUserLocalization")
+        .callsFake((cb) => {
+          setTimeout(() => {
+            cb({
+              coords: {
+                latitude: 41,
+                longitude: 57,
+              },
+            });
+          }, 100);
+        });
     });
+  });
+
+  it("should fetch the user location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getCurrentUserLocalization").should("have.been.called");
+    cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
+    cy.get('[data-cy="actions"]').should("contain", "Location");
+  });
+
+  it("should share the user location URL", () => {
+    cy.get('[data-cy="name-input"]').type("Anne Dave");
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get('[data-cy="location-url"]').should("exist");
   });
 });
