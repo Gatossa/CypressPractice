d6a4e0e203324738030d0f043dde4f46a7fb985f
Gatossa <email@users.noreply.github.com>
2025-02-01 13:06:45 +0100

    API

--- FundamentalsCourse/NetworkRequestsAndDatabases/cypress/e2e/newsletters.cy.js
+++ FundamentalsCourse/NetworkRequestsAndDatabases/cypress/e2e/newsletters.cy.js
@@ -5,10 +5,34 @@ describe("Newsletter", () => {
     cy.task("seedDatabase");
   });
   it("should display a confirmation newsletter message", () => {
-    cy.intercept("POST", "/api/newsletter", { status: 201 });
+    cy.intercept("POST", "/api/newsletter", { status: 201 }).as("subscribe");
     cy.visit("/");
     cy.get('[data-cy="newsletter-email"]').type("test@example.com");
     cy.get('[data-cy="newsletter-submit"]').click();
+    //cy.wait("@subscribe");
     cy.contains("Thanks for signing up!");
+  });
+
+  it("should display validation errors", () => {
+    cy.intercept("POST", "/api/newsletter", {
+      message: "Email exists already.",
+    }).as("subscribe");
+    cy.visit("/");
+    cy.get('[data-cy="newsletter-email"]').type("test@example.com");
+    cy.get('[data-cy="newsletter-submit"]').click();
+    //cy.wait("@subscribe");
+    cy.contains("Email exists already.");
+  });
+
+  it("should create a new contact", () => {
+    cy.request({
+      method: "POST",
+      url: "/newsletter",
+      body: { email: "test@example.com" },
+      form: true,
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property("email", "test@example.com");
+    });
   });
 });
